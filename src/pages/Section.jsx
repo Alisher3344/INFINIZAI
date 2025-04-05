96738dbea7032c595291a7e9a73fa6ede16179e4
Alisher3344 <alisher3344ali@gmail.com>
2025-04-05 21:26:37 +0500

    end Section

--- src/pages/Section.jsx
+++ src/pages/Section.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "../css/Section.css";
 const Section = () => {
   return (
-    <div>Section</div>
-  )
-}
+    <section>
+      <div className="container sectionf">
+        <img className="SecitonLogo" src="section.svg" alt="No logo?" />
+        <p className="sp">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis, nec
+          donec in morbi pulvinar. Enim non pulvinar neque.
+        </p>
+        <div className="imgSection">
+          <img src="IMAGE (4).svg" alt="No img?" />
+          <img src="IMAGE (5).svg" alt="No img?" />
+          <img src="IMAGE (6).svg" alt="No img?" />
+          <img src="IMAGE (7).svg" alt="No img?" />
+          <img src="IMAGE (8).svg" alt="No img?" />
+          <img src="IMAGE (9).svg" alt="No img?" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Section
+export default Section;
