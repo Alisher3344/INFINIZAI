4521b33d83c15b13eeb6fb345d4ae3cf55d850b3
Alisher3344 <alisher3344ali@gmail.com>
2025-04-05 23:03:24 +0500

    exemple chala qoldi

--- src/pages/Aside.jsx
+++ src/pages/Aside.jsx
@@ -1,15 +1,68 @@
 import React from "react";
-
+import "../css/Aside.css";
 const Aside = () => {
   return (
-    <div>
-      <p className="Headerp">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, alias
-        itaque harum ipsam placeat incidunt, dolor dignissimos corrupti possimus
-        obcaecati, illum exercitationem sint illo eveniet accusantium nobis
-        numquam laboriosam! Voluptatum?
-      </p>
-    </div>
+    <aside>
+      <div className="container">
+        <div className="AsideHead">
+          <h4 style={{ margin: "0px " }}>
+            <img src="Fire.svg" alt="No img? " />
+            FUTURE OF eSPORTS
+          </h4>
+          <h3 className="Aside_h3">Your one stop shop gaming needs!</h3>
+          <p className="asideP">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam
+          </p>
+        </div>
+        <div className="GridAside">
+          <div className="Card_Aside">
+            <img src="star.svg" alt="No img?"></img>
+            <div>
+              <h5 className="Aside_h5">All New Gameplay</h5>
+              <p className="Aside_p">
+                Vivamus magna justo, lacinia eget coecetur sed, convallis at
+                tellus. Curabitur non nulla sit Vestibum ante ipsum primis in
+                faucibus orci
+              </p>
+            </div>
+          </div>
+          <div className="Card_Aside">
+            <img src="star.svg" alt="No img?"></img>
+            <div>
+              <h5 className="Aside_h5">Generation Graphics</h5>
+              <p className="Aside_p">
+                Vivamus magna justo, lacinia eget coecetur sed, convallis at
+                tellus. Curabitur non nulla sit Vestibum ante ipsum primis in
+                faucibus orci
+              </p>
+            </div>
+          </div>
+          <div className="Card_Aside">
+            <img src="star.svg" alt="No img?"></img>
+            <div>
+              <h5 className="Aside_h5">Lastest Game Engine</h5>
+              <p className="Aside_p">
+                Vivamus magna justo, lacinia eget coecetur sed, convallis at
+                tellus. Curabitur non nulla sit Vestibum ante ipsum primis in
+                faucibus orci
+              </p>
+            </div>
+          </div>
+          <div className="Card_Aside">
+            <img src="star.svg" alt="No img?"></img>
+            <div>
+              <h5 className="Aside_h5">Multiplayer on the go</h5>
+              <p className="Aside_p">
+                Vivamus magna justo, lacinia eget coecetur sed, convallis at
+                tellus. Curabitur non nulla sit Vestibum ante ipsum primis in
+                faucibus orci
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
   );
 };
 
--- src/pages/Exemple.jsx
+++ src/pages/Exemple.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Exemple = () => {
   return (
-    <div>Exemple</div>
-  )
-}
+    <div className="Exemple">
+      <div className="container">
+        <div>
+          <h4>
+            <img src="Fire.svg" alt="No img? " />
+            FUTURE OF eSPORTS
+          </h4>
+          <h2>The new era of cloud gaming here</h2>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+          </p>
+          <button>Explore More</button>
+        </div>
+        <div className="modal">
+          <img src="play.svg" alt="No icon?" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Exemple
+export default Exemple;
